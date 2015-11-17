98c27ba1cdac7571b60c300431e8076ba9cc8b64
maeda-m <hogehogefoozoobar@gmail.com>
2015-11-17 23:14:32 +0900

    Add test case

--- test/integration/account_with_lockable_test.rb
+++ test/integration/account_with_lockable_test.rb
@@ -6,27 +6,33 @@ class AccountWithLockableTest < Redmine::IntegrationTest
   fixtures :users, :email_addresses, :roles
 
   def test_active_account_of_password_login_user_locked_for_shortly
-    with_settings(allow_failed_attempts_2) do
-      user = User.active.find_by_login('jsmith')
+    normal_user = User.active.find_by_login('jsmith')
+    admin_user = User.active.find_by_login('admin')
 
-      # 1st, 2nd, 3rd => Failed
-      assert_try_login(user, times: 3, locked_times: :last)
-      assert_mail_to_locked_account(user)
+    [normal_user, admin_user].each do |user|
+      with_settings(allow_failed_attempts_2) do
+        # 1st, 2nd, 3rd => Failed
+        assert_try_login(user, times: 3, locked_times: :last)
+        assert_mail_to_locked_account(user)
+      end
     end
   end
 
   def test_active_account_of_password_login_user_locked_finally
-    with_settings(allow_failed_attempts_2) do
-      user = User.active.find_by_login('jsmith')
+    normal_user = User.active.find_by_login('jsmith')
+    admin_user = User.active.find_by_login('admin')
 
-      # 1st, 2nd => Failed
-      assert_try_login(user, times: 2)
-      # 3rd => Success
-      assert_try_login(user, password: user.login)
-      # 4th => Failed
-      post '/logout'
-      assert_try_login(user, times: 3, locked_times: :last)
-      assert_mail_to_locked_account(user)
+    [normal_user, admin_user].each do |user|
+      with_settings(allow_failed_attempts_2) do
+        # 1st, 2nd => Failed
+        assert_try_login(user, times: 2)
+        # 3rd => Success
+        assert_try_login(user, password: user.login)
+        # 4th => Failed
+        post '/logout'
+        assert_try_login(user, times: 3, locked_times: :last)
+        assert_mail_to_locked_account(user)
+      end
     end
   end
 
