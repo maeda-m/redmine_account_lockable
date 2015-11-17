c388472b14e6347718824a6b8bfb23ef5a8ba057
maeda-m <hogehogefoozoobar@gmail.com>
2015-11-17 21:29:30 +0900

    Add send mail test

--- test/integration/account_with_lockable_test.rb
+++ test/integration/account_with_lockable_test.rb
@@ -1,47 +1,65 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class AccountWithLockableTest < Redmine::IntegrationTest
+  include AccountLockable::TestHelper
+
   fixtures :users, :email_addresses, :roles
 
   def test_active_account_of_password_login_user_locked_for_shortly
-    Setting.plugin_redmine_account_lockable[:allow_failed_attempts] = '2'
+    with_settings(allow_failed_attempts_2) do
+      default_admin = User.active.find_by_login('admin')
+      user = User.active.find_by_login('jsmith')
 
-    user = User.find_by_login('jsmith')
-    # 1st => Failed
-    post '/login', username: user.login, password: '1234567'
-    assert_equal false, user.reload.locked?
-    # 2nd => Failed
-    post '/login', username: user.login, password: 'abcedfg'
-    assert_equal false, user.reload.locked?
-    # 3rd => Failed
-    post '/login', username: user.login, password: 'hogehoge'
-    assert_equal true, user.reload.locked?
+      # 1st => Failed
+      post '/login', username: user.login, password: '1234567'
+      assert_equal false, user.reload.locked?
+      # 2nd => Failed
+      post '/login', username: user.login, password: 'abcedfg'
+      assert_equal false, user.reload.locked?
+      # 3rd => Failed
+      post '/login', username: user.login, password: 'hogehoge'
+      assert_equal true, user.reload.locked?
+
+      mail = ActionMailer::Base.deliveries.last
+      assert_not_nil mail
+      assert_equal [user.mail], mail.to
+      assert_equal [default_admin.mail], mail.cc
+      assert_equal mail.subject, l(:text_account_locked_subject, user: user.login)
+    end
   end
 
   def test_active_account_of_password_login_user_locked_finally
-    Setting.plugin_redmine_account_lockable[:allow_failed_attempts] = '2'
+    with_settings(allow_failed_attempts_2) do
+      default_admin = User.active.find_by_login('admin')
+      user = User.active.find_by_login('jsmith')
 
-    user = User.find_by_login('jsmith')
-    # 1st => Failed
-    post '/login', username: user.login, password: '1234567'
-    assert_equal false, user.reload.locked?
-    # 2nd => Failed
-    post '/login', username: user.login, password: 'abcedfg'
-    assert_equal false, user.reload.locked?
-    # 3rd => Success
-    post '/login', username: user.login, password: user.login
-    assert_equal false, user.reload.locked?
+      # 1st => Failed
+      post '/login', username: user.login, password: '1234567'
+      assert_equal false, user.reload.locked?
+      # 2nd => Failed
+      post '/login', username: user.login, password: 'abcedfg'
+      assert_equal false, user.reload.locked?
+      # 3rd => Success
+      post '/login', username: user.login, password: user.login
+      assert_equal false, user.reload.locked?
 
-    # 4th => Failed
-    post '/logout'
-    post '/login', username: user.login, password: ''
-    assert_equal false, user.reload.locked?
-    # 5th => Failed
-    post '/login', username: user.login, password: '1234567'
-    assert_equal false, user.reload.locked?
-    # 6th => Failed
-    post '/login', username: user.login, password: 'abcedfg'
-    assert_equal true, user.reload.locked?
+      # 4th => Failed
+      post '/logout'
+      post '/login', username: user.login, password: ''
+      assert_equal false, user.reload.locked?
+      # 5th => Failed
+      post '/login', username: user.login, password: '1234567'
+      assert_equal false, user.reload.locked?
+      # 6th => Failed
+      post '/login', username: user.login, password: 'abcedfg'
+      assert_equal true, user.reload.locked?
+
+      mail = ActionMailer::Base.deliveries.last
+      assert_not_nil mail
+      assert_equal [user.mail], mail.to
+      assert_equal [default_admin.mail], mail.cc
+      assert_equal mail.subject, l(:text_account_locked_subject, user: user.login)
+    end
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,20 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 module AccountLockable
   module TestHelper
     include Redmine::I18n
+
+    def default_redmine_settings
+      {
+        bcc_recipients: '0',
+        default_language: 'en'
+      }
+    end
+
+    def allow_failed_attempts_2
+      default_redmine_settings.merge(
+        plugin_redmine_account_lockable: {
+          allow_failed_attempts: '2'
+        })
+    end
+
   end
 end
